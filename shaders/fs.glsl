e4521ed4b61ff47f8594846bc6890a1cc06b4c3e
august van Casteren <augustvc@outlook.com>
2017-06-26 20:04:03 +0200

    shaders updated with transforms

--- shaders/fs.glsl
+++ shaders/fs.glsl
@@ -7,9 +7,15 @@ in vec4 intersectionPoint;		// the world location of the pixel you are drawing
 uniform sampler2D pixels;		// texture sampler
 uniform vec4 ambientColor;		// color that's added to every value of light
 uniform vec3 cameraPos;			// position of the camera at the time of drawing this.
+
+uniform int lightAmount;
+uniform vec3 lightPos[lightAmount];
+uniform vec3 lightIntensity[lightAmount];
+uniform vec3 lightSpecular[lightAmount];
+
 vec3 lightpos1 = vec3(0, -800, 0);
-vec3 light1 = vec3(1000000, 1000000, 1000000);		
-vec3 lightSpec1 = vec3(1000000, 1000000, 1000000);		
+vec3 light1 = vec3(300000, 300000, 300000);		
+vec3 lightSpec1 = vec3(1, 1, 1);		
 
 // shader output
 out vec4 outputColor;
@@ -24,14 +30,14 @@ void main()
 	vec3 L = lightpos1 - intersectionPoint.xyz;
 	vec3 C = normalize(intersectionPoint.xyz - cameraPos); // supposed to be a normalized version of the vector from the camera to the intersection point, reflected in the normal of the surface of the reflection point
 	
-	float ndotl =  dot(normalize(L), normal.xyz);
+	float ndotl = dot(normalize(L), normal.xyz);
 	ndotl = max(ndotl, -ndotl);
-	vec3 mirroredL = normalize(L - (2 * normal.xyz * dot(normal.xyz,L)));
+	vec3 mirroredL = normalize(L + (2 * normal.xyz * dot(normal.xyz,L)));
 
 	vec4 diffTexture = vec4(light1.x * texture( pixels, uv ).x, light1.y * texture( pixels, uv).y, light1.z * texture( pixels, uv ).z, 0);
 	vec4 specularTexture = vec4(lightSpec1.x * texture( pixels, uv ).x, lightSpec1.y * texture( pixels, uv).y, lightSpec1.z * texture( pixels, uv ).z, 0);
 
-	float specular = pow(dot(mirroredL,C),1200);
+	float specular = pow(dot(mirroredL,C),1000);
 
 	vec4 cDiff = (ndotl * diffTexture) / (L.x * L.x + L.y * L.y + L.z * L.z);
 	vec4 cSpecular = specular * specularTexture;
